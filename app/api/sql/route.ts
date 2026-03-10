16f21df1a934264a72c1158fffe28e7d70c10bfb
idea-ai-labs <idea.ai.labs@gmail.com>
2026-03-09 21:23:33 -0400

    Update route.ts

--- app/api/sql/route.ts
+++ app/api/sql/route.ts
@@ -1,38 +1,16 @@
 import { NextResponse } from "next/server"
-import sqlite3 from "sqlite3"
-import { open } from "sqlite"
+import { sql } from "@vercel/postgres"
 
 export async function POST(req: Request) {
   try {
     const body = await req.json()
-    const { query, password } = body
-    /*
-    if (password !== process.env.SQL_EDITOR_PASSWORD) {
-      return NextResponse.json({
-        success: false,
-        error: "Unauthorized"
-      })
-    }
-    */
+    const { query } = body
 
-    const db = await open({
-      filename: "./data.db",
-      driver: sqlite3.Database
-    })
-
-    const trimmed = query.trim().toLowerCase()
-
-    let result
-
-    if (trimmed.startsWith("select") || trimmed.startsWith("pragma")) {
-      result = await db.all(query)
-    } else {
-      result = await db.run(query)
-    }
+    const result = await sql.query(query)
 
     return NextResponse.json({
       success: true,
-      result
+      rows: result.rows
     })
 
   } catch (err: any) {
